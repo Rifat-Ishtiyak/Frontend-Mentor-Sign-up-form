cf89444797f6bc42483067847b393a240802c7fe
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2022-02-15 21:25:53 +0600

    validation updated

--- app.js
+++ app.js
@@ -14,13 +14,36 @@ document.getElementById('submit-btn').addEventListener('click', function(){
         document.getElementById('email-error').innerText='Email cannot be empty'
         document.getElementById('email-field').style.border='2px solid red';
     }
-    if((!document.getElementById('email-field').value.includes('@') || !document.getElementById('email-field').value.includes('.')) && !document.getElementById('email-field').value==''){
-        document.getElementById('email-error').innerText='Looks like this is not an email'
+    if((!document.getElementById('email-field').value.includes('@') || !document.getElementById('email-field').value.includes('.com')) && !document.getElementById('email-field').value==''){
+        document.getElementById('email-error').innerText='Looks like this is not an email';
+        document.getElementById('email-field').style.border='2px solid red';
+        document.getElementById('warning-email').style.display='block';
     }
 
     if(document.getElementById('pass-field').value==''){
         document.getElementById('warning-pass').style.display='block';
         document.getElementById('pass-error').innerText='Password cannot be empty'
         document.getElementById('pass-field').style.border='2px solid red';
+    }
+
+    if (document.getElementById('first-name').value != '') {
+        document.getElementById('warning-name').style.display='none';
+        document.getElementById('name-error').innerText='';
+        document.getElementById('first-name').style.border='2px solid green';
+    }
+    if(document.getElementById('second-name').value != ''){
+        document.getElementById('warning-last-name').style.display='none';
+        document.getElementById('second-error').innerText='';
+        document.getElementById('second-name').style.border='2px solid green';
+    }
+    if(document.getElementById('email-field').value != '' && document.getElementById('email-field').value.includes('@') && document.getElementById('email-field').value.includes('.')){
+        document.getElementById('warning-email').style.display='none';
+        document.getElementById('email-error').innerText=''
+        document.getElementById('email-field').style.border='2px solid green';
+    }
+    if(document.getElementById('pass-field').value !=''){
+        document.getElementById('warning-pass').style.display='none';
+        document.getElementById('pass-error').innerText=''
+        document.getElementById('pass-field').style.border='2px solid green';
     }
 })
